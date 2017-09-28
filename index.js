97463d68354a22ae0b8a93ebcb4ad7c8068323a6
billscarp <billscarp@gmail.com>
2017-09-27 22:08:03 -0400

    Gave better response to questions

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function checkAnswers(type, answers) {
 
 		basic_questions.forEach(function(question, index) {
 			if ( answers[keys[index]] == question.back ) {
-				console.log('correct');
+				console.log('Good job, you are correct! ');
 			}else{
-                console.log('incorrect');
+                console.log('Sorry, this is incorrect.');
             }
 		});
 	}
